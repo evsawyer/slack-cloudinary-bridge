6b505c3365f86d5f30470620c10daa5195ffa830
Evan Sawyer @ IVC <evan@ivc.media>
2025-04-16 11:02:56 -0700

    from fastmcp import FastMCP

--- slack_cloudinary_bridge/slack_cloudinary_bridge.py
+++ slack_cloudinary_bridge/slack_cloudinary_bridge.py
@@ -1,4 +1,4 @@
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
 # from .slack_helper import download_slack_image
 # from .cloudinary_helper import upload_to_cloudinary
 import os
